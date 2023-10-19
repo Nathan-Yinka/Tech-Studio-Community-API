5e63bd6a100a7294ee481dd55278f9de1bfe627e
Nathan-Yinka <oludarenathaniel@gmail.com>
2023-10-19 22:02:46 +0100

    added email link

--- users/utilis.py
+++ users/utilis.py
@@ -31,8 +31,9 @@ def send_confirmation_email(user, request):
             'token': signed_token,
         }
         confirmation_link = reverse('auth:confirm-email')
-        confirmation_url = request.build_absolute_uri(confirmation_link)
-        confirmation_url = f'{confirmation_url}{signed_user_id}/{signed_token}/'
+        confirmation_link = "https://tech-studio-community-frontend-ngf7-nathan-yinka.vercel.app/"
+        # confirmation_url = request.build_absolute_uri(confirmation_link)
+        confirmation_url = f'{confirmation_link}{signed_user_id}/{signed_token}/'
 
         # subject = "Confirm Your Email Address"
         # message = f"Click the link below to confirm your email address:\n\n{confirmation_url}"
