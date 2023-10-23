8498d4f387673ee1cec05a6e55778d1474adaf21
Nathan-Yinka <oludarenathaniel@gmail.com>
2023-10-23 10:55:13 +0100

    update the IsOwerOrReadOnly Class

--- api/permissions.py
+++ api/permissions.py
@@ -1,4 +1,8 @@
 from rest_framework import permissions
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
@@ -7,13 +11,19 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
+    # Read permissions are allowed to any request,
+    # so we'll always allow GET, HEAD, or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Instance must have an attribute named `owner`.
-        return obj.id == request.user.id
+        # Check if the object is an instance of the User model
+        if isinstance(obj, User):
+            # If it's a User object, check if obj.id matches request.user.id
+            return obj.id == request.user.id
+        else:
+            # For other object types, check if obj.user.id matches request.user.id
+            return obj.user.id == request.user.id
+
     
 class IsStaffOrReadOnly(permissions.BasePermission):
     """
